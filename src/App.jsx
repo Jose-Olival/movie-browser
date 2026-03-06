e61a946154362f466193e393d07f1733a1127e31
Jose Olival <joseandresolival@gmail.com>
2026-03-06 16:55:22 -0400

    header css and html

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="page">
+      <header>
+        <h1>Buscador de Peliculas</h1>
+        <form className="search-bar">
+          <input type="text" placeholder="Superman, The Sinners, The Lord Of The Rings..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+      <main>
+        Aqui las Peliculas
+      </main>
+    </div>
   )
 }
 
