ba9bed663af1f4b480bd590334c8021e31b32879
Jose Olival <joseandresolival@gmail.com>
2026-03-11 12:00:47 -0400

    forma controlado implementada

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import { useMovies } from './hooks/useMovies'
 function App() {
   const { movies } = useMovies()
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const { query } = Object.fromEntries(new window.FormData(event.target))
+    console.log(query)
+  }
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de Peliculas</h1>
-        <form className="search-bar">
-          <input type="text" placeholder="Superman, The Sinners, The Lord Of The Rings..." />
-          <button type="submit">Buscar</button>
+        <form className="search-bar" onSubmit={handleSubmit}>
+          <input name='query' type="text" placeholder="Superman, The Sinners, The Lord Of The Rings..." />
+          <button type='submit'>Buscar</button>
         </form>
       </header>
       <main>
