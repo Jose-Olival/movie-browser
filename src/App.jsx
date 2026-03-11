91957d1264816553d29fd717d96e5385c90b94e4
Jose Olival <joseandresolival@gmail.com>
2026-03-11 16:04:05 -0400

    useSeatch created, forma controlada

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useRef, useState } from 'react'
 import MovieList from './componets/MovieList'
 import { useMovies } from './hooks/useMovies'
 
+function useSearch () {
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = query === ''
+      return
+    }
+
+    if (query === ' ') {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setError('No se puede realizar busquedas con espacio en blanco')
+      return
+    }
+
+    if (query.length < 3) {
+      setError('No se puede realizar busquedas con menos de 3 caracteres')
+      return
+    }
+
+    setError(null)
+
+  }, [query])
+
+  const updateQuery = (event) => {
+    if (event.target.value.startsWith(' ')) return
+    setQuery(event.target.value)
+  }
+
+  return {query, updateQuery, error}
+}
+
 function App() {
-  const { movies } = useMovies()
+  const { movies } = useMovies() 
+  const {query, updateQuery, error} = useSearch()
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -15,9 +51,10 @@ function App() {
       <header>
         <h1>Buscador de Peliculas</h1>
         <form className="search-bar" onSubmit={handleSubmit}>
-          <input name='query' type="text" placeholder="Superman, The Sinners, The Lord Of The Rings..." />
+          <input value={query} onChange={updateQuery} name='query' type="text" placeholder="Superman, The Sinners, The Lord Of The Rings..." />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{color: '#a11'}}>{error}</p>}
       </header>
       <main>
         {
