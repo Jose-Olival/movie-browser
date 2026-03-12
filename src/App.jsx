54db6181f51eab94e09e50735450dcefd682ff12
Jose Olival <joseandresolival@gmail.com>
2026-03-12 14:50:40 -0400

    sort by year checkbox working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import MovieList from './componets/MovieList'
+import { useState } from 'react'
 import { useSearch } from './hooks/useSearch'
 import { useMovies } from './hooks/useMovies'
+import MovieList from './componets/MovieList'
 
 
 function App() {
+  const [sort, setSort] = useState(false)
   const {query, updateQuery, error} = useSearch()
-  const { movies, getMovies } = useMovies({ query }) 
+  const { movies, getMovies } = useMovies({ query, sort }) 
 
   const handleSubmit = (event) => {
     event.preventDefault()
     getMovies()
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
   }
 
   return (
@@ -18,6 +24,7 @@ function App() {
         <h1>Buscador de Peliculas</h1>
         <form className="search-bar" onSubmit={handleSubmit}>
           <input value={query} onChange={updateQuery} name='query' type="text" placeholder="Superman, The Sinners, The Lord Of The Rings..." />
+          <input type="checkbox" value={sort} name="sort" onChange={handleSort}/>
           <button type='submit'>Buscar</button>
         </form>
         {error && <p style={{color: '#a11'}}>{error}</p>}
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,14 +1,30 @@
 import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
-export function useMovies ({ query }) {
+export function useMovies ({ query, sort }) {
   const [movies, setMovies] = useState([])
   const previousQuery = useRef('')
+
+  const getReleaseYear = (movie) => {
+    return Number(movie.year.slice(0,4))
+  }
+  
+  const sortMovies = (moviesToSort) => {
+    return moviesToSort.sort((a,b) => {
+      const yearA = getReleaseYear(a)
+      const yearB = getReleaseYear(b)
+      return yearA > yearB ?  -1 
+      : yearB > yearA ? 1 
+      : 0
+    })
+    
+  }
 
   const getMovies = async () => { 
     if (query === previousQuery.current) return
     previousQuery.current = query
-    const newMovies = await searchMovies({query})
+    const rawMovies = await searchMovies({query})
+    const newMovies = sort ? sortMovies(rawMovies) : rawMovies
     setMovies(newMovies)
   }
 
