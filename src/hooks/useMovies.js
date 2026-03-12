58d94cec87caee8b2f20171e25d263de63e4147b
Jose Olival <joseandresolival@gmail.com>
2026-03-12 13:40:22 -0400

    avoid searching the duplicated query

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ query }) {
   const [movies, setMovies] = useState([])
+  const previousQuery = useRef('')
 
   const getMovies = async () => { 
+    if (query === previousQuery.current) return
+    previousQuery.current = query
     const newMovies = await searchMovies({query})
     setMovies(newMovies)
   }
